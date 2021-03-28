882510db8bba616bec1485fa33c161b557bd06bd
Fraser Carter <fraserc97@hotmail.co.uk>
2021-03-28 14:15:26 +0100

    updated config files

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    "sauce/test/":"/",
+    "sauce/": "/sauce/",
   },
   plugins: [
     ['@snowpack/plugin-typescript', { tsc: "tsc" }], 
